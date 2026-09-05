92400a2fa5dc83e73d69765647d39126dad26c85
Dmytro Patseliuk <284061079+patseluk-lang@users.noreply.github.com>
2026-09-05 09:34:59 +0300

    Update database.py

--- Cinema/database.py
+++ Cinema/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import psycopg2
 
-DATABASE_URL = "postgresql+psycopg2://patselukserv:d29011972@localhost:5433/online_cinema"
+DATABASE_URL = "postgresql+psycopg2://patselukserv:parol@localhost:5433/online_cinema"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
